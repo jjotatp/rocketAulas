7580bc36848be40fbac49984516c4ec0d4af777e
jjotatp <juniorjtp@hotmail.com>
2020-02-24 15:35:43 -0300

    Fim do módulo 1 de ES6

--- ES6/bundle.js
+++ ES6/bundle.js
@@ -1,27 +1,106 @@
 "use strict";
 
 //=======================================================
+// OBJECT SHORT SYNTAX
+//=======================================================
+var nome = "Jairo";
+var idade = 24;
+var user = {
+  nome: nome,
+  idade: idade,
+  empresa: "Frog Digital"
+};
+console.log(user); //=======================================================
+// TEMPLATE LITERALS
+//=======================================================
+// const nome = "Jairo";
+// const idade = 24;
+// console.log(`Meu nome é ${nome} e tenho ${idade} anos!`);
+//=======================================================
+// Rest e Spread
+//=======================================================
+//REST
+// const arr = [1, 2, 3, 4, 5];
+// const [a, b, ...c] = arr;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// function soma(...params){
+// 	return params.reduce((total, next) => total + next);
+// }
+// console.log(soma(1, 2, 3, 4, 5));
+// //SPREAD
+// const arr1 = [1, 2, 3];
+// const arr2 = [4, 5, 6];
+// const arr3 = [...arr1, ...arr2];
+// console.log(arr3);
+// const user = {
+// 	nome: 'Jairo',
+// 	idade: 24,
+// 	cidade: 'SP',
+// };
+// const user2 = { ...user, nome: 'Jota'};
+// console.log(user);
+// console.log(user2);
+//=======================================================
+// DESESTRUTURAÇÃO
+//=======================================================
+// const usuario = {
+// 	nome: 'Jairo',
+// 	idade: 24,
+// 	endereco: {
+// 		rua: 'Augusto Tolle',
+// 		numero: 122,
+// 		cidade: 'SP',
+// 	},
+// };
+// // const {nome, idade, endereco: {cidade}} = usuario;
+// // console.log(nome);
+// // console.log(idade);
+// // console.log(cidade);
+// function mostraNome({ nome, idade }) {
+// 	console.log(nome, idade);
+// }
+// mostraNome(usuario);
+// alert("Teste");
+//=======================================================
+// VALORES PADRÃO
+//=======================================================
+// const soma = (a = 3, b = 6) => a + b;
+// console.log(soma(1));
+// console.log(soma());
+//=======================================================
+// ARROWS FUNCTIONS
+//=======================================================
+// const arr = [1, 3, 4, 5, 8, 9];
+// // const newarr = arr.map((item) => {
+// // 	return item * 2;
+// // });
+// const newarr = arr.map(item => item * 2);
+// console.log(newarr);
+//=======================================================
 // ARRAYS E VETORES
 //=======================================================
-var arr = [1, 3, 4, 5, 8, 9]; // const newarr = arr.map(function (item) {
-// return item * 2;
-
-var newarr = arr.map(function (item, index) {
-  return item * index;
-});
-console.log(newarr);
-var sum = arr.reduce(function (total, next) {
-  return total + next;
-});
-console.log('O total do vetor é = ' + sum);
-var filter = arr.filter(function (item) {
-  return item % 2 === 0;
-});
-console.log(filter);
-var find = arr.find(function (item) {
-  return item === 4;
-});
-console.log(find); //=======================================================
+// const arr = [1, 3, 4, 5, 8, 9];
+// // const newarr = arr.map(function (item) {
+// 	// return item * 2;
+// const newarr = arr.map(function (item,index) {
+// 	return item * index;
+// });
+// console.log(newarr);
+// const sum = arr.reduce(function (total, next){
+// 	return total + next;
+// });
+// console.log('O total do vetor é = ' + sum);
+// const filter = arr.filter(function (item){
+// 	return item % 2 === 0;
+// });
+// console.log(filter);
+// const find = arr.find(function(item){
+// 	return item === 4;
+// });
+// console.log(find);
+//=======================================================
 // 							CLASSES
 //=======================================================
 // class TodoList {
--- ES6/main.js
+++ ES6/main.js
@@ -1,34 +1,146 @@
 //=======================================================
+// OBJECT SHORT SYNTAX
+//=======================================================
+
+const nome = "Jairo";
+const idade = 24;
+
+const user = {
+	nome,
+	idade,
+	empresa: "Frog Digital",
+};
+
+console.log(user);
+//=======================================================
+// TEMPLATE LITERALS
+//=======================================================
+
+// const nome = "Jairo";
+// const idade = 24;
+
+// console.log(`Meu nome é ${nome} e tenho ${idade} anos!`);
+
+//=======================================================
+// Rest e Spread
+//=======================================================
+
+//REST
+// const arr = [1, 2, 3, 4, 5];
+
+// const [a, b, ...c] = arr;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+// function soma(...params){
+// 	return params.reduce((total, next) => total + next);
+// }
+
+// console.log(soma(1, 2, 3, 4, 5));
+
+// //SPREAD
+
+// const arr1 = [1, 2, 3];
+// const arr2 = [4, 5, 6];
+// const arr3 = [...arr1, ...arr2];
+// console.log(arr3);
+
+// const user = {
+// 	nome: 'Jairo',
+// 	idade: 24,
+// 	cidade: 'SP',
+// };
+
+// const user2 = { ...user, nome: 'Jota'};
+
+// console.log(user);
+// console.log(user2);
+
+//=======================================================
+// DESESTRUTURAÇÃO
+//=======================================================
+
+// const usuario = {
+// 	nome: 'Jairo',
+// 	idade: 24,
+// 	endereco: {
+// 		rua: 'Augusto Tolle',
+// 		numero: 122,
+// 		cidade: 'SP',
+// 	},
+// };
+
+// // const {nome, idade, endereco: {cidade}} = usuario;
+
+// // console.log(nome);
+// // console.log(idade);
+// // console.log(cidade);
+
+// function mostraNome({ nome, idade }) {
+// 	console.log(nome, idade);
+// }
+
+// mostraNome(usuario);
+// alert("Teste");
+
+//=======================================================
+// VALORES PADRÃO
+//=======================================================
+
+// const soma = (a = 3, b = 6) => a + b;
+
+// console.log(soma(1));
+// console.log(soma());
+
+//=======================================================
+// ARROWS FUNCTIONS
+//=======================================================
+
+// const arr = [1, 3, 4, 5, 8, 9];
+
+// // const newarr = arr.map((item) => {
+// // 	return item * 2;
+// // });
+
+// const newarr = arr.map(item => item * 2);
+
+
+// console.log(newarr);
+
+//=======================================================
 // ARRAYS E VETORES
 //=======================================================
 
-const arr = [1, 3, 4, 5, 8, 9];
+// const arr = [1, 3, 4, 5, 8, 9];
 
-// const newarr = arr.map(function (item) {
-	// return item * 2;
-const newarr = arr.map(function (item,index) {
-	return item * index;
-});
+// // const newarr = arr.map(function (item) {
+// 	// return item * 2;
+// const newarr = arr.map(function (item,index) {
+// 	return item * index;
+// });
 
-console.log(newarr);
+// console.log(newarr);
 
-const sum = arr.reduce(function (total, next){
-	return total + next;
-});
+// const sum = arr.reduce(function (total, next){
+// 	return total + next;
+// });
 
-console.log('O total do vetor é = ' + sum);
+// console.log('O total do vetor é = ' + sum);
 
-const filter = arr.filter(function (item){
-	return item % 2 === 0;
-});
+// const filter = arr.filter(function (item){
+// 	return item % 2 === 0;
+// });
 
-console.log(filter);
+// console.log(filter);
 
-const find = arr.find(function(item){
-	return item === 4;
-});
+// const find = arr.find(function(item){
+// 	return item === 4;
+// });
 
-console.log(find);
+// console.log(find);
 
 //=======================================================
 // 							CLASSES
